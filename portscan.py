1966b10dab715ff84139e8a5e7930e063f43780a
BillSpahn <bills@c-devtech.com>
2018-04-14 14:05:36 -0600

    Version 0.2 of portscan code. Added support for port descriptions display based on mephux/ports.json.

--- portscan.py
+++ portscan.py
@@ -1,13 +1,25 @@
 #!python3
 # This program demonstrates the use of sockets in python by scanning ports.
 
+import json
 import logging
 import socket
+import sys
 import threading
 from queue import Queue
 
+def loadPortsFile():
+    try:
+        with open("ports.json") as settings_file:
+            ports = json.load(settings_file)
+            return ports
+    except:
+        print('Error: ', sys.exc_info()[0], ' - ', sys.exc_info()[1])
+        return None
+
+
 # Scan a single an computer for a single of ports.
-def portscan(target, port):
+def portscan(target, port_no):
     s = None
     try:
         # Create new socket for each port tested.
@@ -16,7 +28,7 @@ def portscan(target, port):
         s.settimeout(0.5)
         # Reuse a local socket in TIME_WAIT state.
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.connect((target, port))
+        s.connect((target, port_no))
         s.shutdown(socket.SHUT_RDWR)
         return True
     except:
@@ -26,15 +38,35 @@ def portscan(target, port):
             s.close()
 
 # Worker for multi-threaded port scan
-def worker(print_lock, q):
+def worker(print_lock, ports, q):
     while True:
         # Block until an item is available.
-        (target, port) = q.get()
-        if target is None or port is None:
+        (target, port_no) = q.get()
+        if target is None or port_no is None:
             break
-        if portscan(target, port):
-            with print_lock:
-                print("\tPort", port, "is open!")
+        if portscan(target, port_no):
+            port_str = str(port_no)
+            try:
+                if str(port_str) in ports.keys():
+                    port = ports[port_str]
+                    if type(port) is dict:
+                        if port['tcp'] == True:
+                            description = '{'+port['description']+'}'
+                    elif type(port) is list:
+                        description = ''
+                        for dic in port:
+                            if dic['tcp'] == True:
+                                description += '{'+dic['description']+'} or '
+                        if len(description) > 3:
+                            description = description[:-3]
+                    else:
+                        description = '{Unknown}'
+                else:
+                    description  = '{Unknown}'
+                with print_lock:
+                    print('   Port %s is open! \t\t%s' % (port_no, description))
+            except:
+                pass
         # If a join() is currently blocking, will resume when all
         # items have been processed.
         q.task_done()
@@ -44,11 +76,14 @@ def scanports(target, start_port, end_port):
     # Thread safe queue dependent on threading module.
     q = Queue()
     print_lock = threading.Lock()
+    # Load ports file
+    ports_list = loadPortsFile()
 
     # Create and start the worker thread objects.
     worker_threads = []                 # A list of all the Thread objects
     for i in range(100):                # Number of worker threads.
-        worker_thread = threading.Thread(target=worker, args=(print_lock, q))
+        worker_thread = threading.Thread(target=worker, \
+                args=(print_lock, ports_list['ports'], q))
         worker_thread.daemon = True     # Will die if/when main thread dies.
         worker_threads.append(worker_thread)
         worker_thread.start()
@@ -73,11 +108,14 @@ def subnet_scanports(subnet, start_port, end_port):
     # Thread safe queue dependent on threading module.
     q = Queue()
     print_lock = threading.Lock()
+    # Load ports file
+    ports_list = loadPortsFile()
 
     # Create and start the worker thread objects.
     worker_threads = []                     # A list of all the Thread objects
     for i in range(500):                    # Number of worker threads.
-        worker_thread = threading.Thread(target=worker, args=(print_lock, q))
+        worker_thread = threading.Thread(target=worker, \
+                args=(print_lock, ports_list['ports'], q))
         worker_threads.append(worker_thread)
         worker_thread.start()
 
@@ -102,8 +140,10 @@ def subnet_scanports(subnet, start_port, end_port):
 #                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Examples of using scans
-# subnet_scanports('192.168.1.', 1, 65556)
+subnet_scanports('192.168.1.', 1, 65556)
 # scanports('127.0.0.1', 1, 1000)
-scanports('pythonprogramming.net', 1, 65556)
+# scanports('pythonprogramming.net', 1, 65556)
+
+
 
 
